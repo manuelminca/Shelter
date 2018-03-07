97a3aba90303d15fbf8cbd6a102ab09851185583
Unknown <manuelinf12@gmail.com>
2018-03-07 19:14:04 +0100

    cosas

--- Shelter/src/shelter/ConexionServidor.java
+++ Shelter/src/shelter/ConexionServidor.java
@@ -3,7 +3,6 @@ package shelter;
 import aux.ObjetoEnvio;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -83,5 +82,6 @@ public class ConexionServidor implements ActionListener {
         escribirSocket(socket, objeto);
         tfMensaje.setText("");
     }
+    
 
 }
--- Shelter/src/shelter/Mensaje.java
+++ Shelter/src/shelter/Mensaje.java
@@ -12,6 +12,7 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import javax.swing.JTextArea;
+import static shelter.AES.doEncryptedAES;
 
 /**
  *
